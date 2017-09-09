e3dd8b54b2968589b8d0532ac9771f3371b36990
Jan Brodda <github@janbrodda.de>
2017-09-09 10:05:55 +0200

    removed obsolete parameters and added database connection

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: $(composer config bin-dir)/heroku-php-nginx -C heroku_nginx.conf web/
+web: composer warmup && $(composer config bin-dir)/heroku-php-nginx -C heroku_nginx.conf web/
--- web/index.php
+++ web/index.php
@@ -1,5 +1,12 @@
 <?php
 
+use Symfony\Component\HttpFoundation\Request;
+
+require_once "../vendor/autoload.php";
+
+/** @noinspection PhpUndefinedFieldInspection */
+Request::setTrustedProxies(array($request->server->get('REMOTE_ADDR')), Request::HEADER_X_FORWARDED_FOR);
+
 switch ($_ENV['SYMFONY_ENV']){
     case 'prod':
         require_once 'app.php';
